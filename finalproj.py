6b69f5f25fe4be0124a42cf3d52705363effa992
Gabby Truong <gtruong@Gabbys-MacBook-Pro.local>
2018-04-18 22:49:27 -0400

    working caches

--- finalproj.py
+++ finalproj.py
@@ -6,16 +6,20 @@ import unittest
 import requests
 import secrets
 from yelpapi import YelpAPI
-import argparse
 import sqlite3 as sqlite
+
+
+yelp_api = YelpAPI(secrets.API_KEY)
+
 
 #CACHE FOR NETSTATE
 CACHE_FNAME_3 = 'netstate_cache.json'
+
 try:
     cache_file_3 = open(CACHE_FNAME_3, 'r')
-    cache_contents_3 = cache_file_2.read()
+    cache_contents_3 = cache_file_3.read()
     CACHE_DICT_3 = json.loads(cache_contents_3)
-    cache_file_2.close()
+    cache_file_3.close()
 except:
     CACHE_DICT_3 = {}
 
@@ -25,100 +29,47 @@ def get_cached_netstate(baseurl):
         return CACHE_DICT_3[unique_ident]
     else:
         resp = requests.get(baseurl)
-        CACHE_DICT_3[unique_ident] = json.loads(resp.text)
+        CACHE_DICT_3[unique_ident] = resp.text
         f = open(CACHE_FNAME_3, 'w')
         dumped_json = json.dumps(CACHE_DICT_3)
         f.write(dumped_json)
         f.close()
         return CACHE_DICT_3[unique_ident]
 
-#GET DATA
-#Scrape data from netstate site and get top city in each state / population
-init_user_inp = input('Enter a state abbreviation(type help if needed): ')
+#GET NETSTATE DATA
+class NetState:
+    def __init__(self, city = 'No city', state_abbr = 'No state',
+    population = 'No population', baseurl =None):
+        self.city = city
+        self.state_abbr = state_abbr
+        self.population = population
+        self.baseurl = baseurl
 
+        #self.get_netstate_data(baseurl) #do i have to do url = none?
 
-if init_user_inp == 'help':
-    print('Please enter one of the following state abbreviations:')
-    print('ak for Alaska')
-    print('al for Alabama')
-    print('ar for Arkansas')
-    print('az for Arizona')
-    print('ca for California')
-    print('co for Colorado')
-    print('ct for Connecticut')
-    print('dc for District of Columbia')
-    print('de for Delaware')
-    print('fl for Florida')
-    print('ga for Georgia')
-    print('hi for Hawaii')
-    print('ia for Iowa')
-    print('id for Idaho')
-    print('il for Illinois')
-    print('in for Indiana')
-    print('ks for Kansas')
-    print('ky for Kentucky')
-    print('la for Louisiana')
-    print('ma for Massachusetts')
-    print('md for Maryland')
-    print('me for Maine')
-    print('mi for Michigan')
-    print('mn for Minnesota')
-    print('mo for Missouri')
-    print('ms for Mississippi')
-    print('mt for Montana')
-    print('nc for North Carolina')
-    print('nd for North Dakota')
-    print('ne for Nebraska')
-    print('nh for New Hampshire')
-    print('nj for New Jersey')
-    print('nm for New Mexico')
-    print('nv for Nevada')
-    print('ny for New York')
-    print('oh for Ohio')
-    print('ok for Oklahoma')
-    print('or for Oregon')
-    print('pa for Pennsylvania')
-    print('ri for Rhode Island')
-    print('sc for South Carolina')
-    print('sd for South Dakota')
-    print('tn for Tennessee')
-    print('tx for Texas')
-    print('ut for Utah')
-    print('va for Virginia')
-    print('vt for Vermont')
-    print('wa for Washington')
-    print('wi for Wisconsin')
-    print('wv for West Virginia')
-    print('wy for Wyoming')
-    init_user_inp = input('Enter a state abbreviation: ')
-else:
-    exit()
+    def __str__(self):
+        '{}, {}: population of {}'.format(city, state_abbr.upper(), population)
 
-def get_netstate_data(baseurl, state_abbr):
+def get_netstate_data(state_abbr): #do i take other params
     init_user_inp = state_abbr
-    #baseurl = 'http://www.netstate.com/states/alma/{}_alma.htm'.format(state_abbr)
-    #html = get_cached_netstate(baseurl).text
     baseurl = 'http://www.netstate.com/states/alma/{}_alma.htm'.format(state_abbr)
-    resp = requests.get(baseurl).text
-    soup = BeautifulSoup(resp, 'html.parser') #change resp to html when figured out cache
-    soup.prettify()
-    symbol_table =soup.find(id="symboltable")
+    html = get_cached_netstate(baseurl)
+    soup = BeautifulSoup(html, 'html.parser')
+    symbol_table =soup.find(id='symboltable')
     div = symbol_table.find('table')
     my_table= div.find('table')
-    #print(my_table)
     tr = my_table.find_all('tr')[0]
-    #print(tr)
     td = tr.find_all('td')[1:3]
-    #print(td)
     city_list = []
+
     for item in td:
-        print(item.string)
-            #command_resp = ((item.string)[0] + ', {}:' + (item.string)[1]).format(state_abbr)
+        print(item.string) #was printing city, population under it
+                #command_resp = ((item.string)[0] + ', {}:' + (item.string)[1]).format(state_abbr)
 
-#while user_inp != 'exit':
-#    get_netstate_data({}).format(user_inp)
+get_netstate_data(baseurl, init_user_inp)
 
-yelp_api = YelpAPI(secrets.API_KEY)
+
+
 
 #YELP CACHE FOOD
 search_results = yelp_api.search_query(term = 'Restaurant', location = 'Birmingham, AL') #.format(city) #add state
@@ -132,13 +83,13 @@ with open('yelp_cache.json') as json_data:
 list_of_restaurants = []
 for x in range(0, 5):
     list_of_restaurants.append(cached_file["businesses"][x])
-    rating = cached_file["businesses"][x]["rating"]
-    price = cached_file["businesses"][x]["price"]
-    review_num = cached_file["businesses"][x]["review_count"]
-    name = cached_file["businesses"][x]["alias"]
-    loc = cached_file["businesses"][x]["location"]["display_address"]
+    r_rating = cached_file["businesses"][x]["rating"]
+    r_price = cached_file["businesses"][x]["price"]
+    r_review_num = cached_file["businesses"][x]["review_count"]
+    r_name = cached_file["businesses"][x]["alias"]
+    r_loc = cached_file["businesses"][x]["location"]["display_address"]
     print('----------------------restaurant--------------------------')
-    print(name.replace('-', ' ') + ' ' + str(loc))
+    print(r_name.replace('-', ' ') + ' ' + str(r_loc))
     print('Rating: ' + str(rating))
     print('Number of reviews: ' + str(review_num))
     print('Price range: ' + str(price))
@@ -154,22 +105,17 @@ with open('yelp_cache_2.json') as json_data_2:
 list_of_hotels = []
 for x in range(0, 5):
     list_of_hotels.append(cached_file_2["businesses"][x])
-    rating = cached_file_2["businesses"][x]["rating"]
-    price = cached_file_2["businesses"][x]["price"]
-    review_num = cached_file_2["businesses"][x]["review_count"]
-    name = cached_file_2["businesses"][x]["alias"]
-    loc = cached_file_2["businesses"][x]["location"]["display_address"]
+    h_rating = cached_file_2["businesses"][x]["rating"]
+    h_price = cached_file_2["businesses"][x]["price"]
+    h_review_num = cached_file_2["businesses"][x]["review_count"]
+    h_name = cached_file_2["businesses"][x]["alias"]
+    h_loc = cached_file_2["businesses"][x]["location"]["display_address"]
     print('-----------------------hotel---------------------------')
-    print(name.replace('-', ' ') + ' ' + str(loc))
-    print('Rating: ' + str(rating))
-    print('Number of reviews: ' + str(review_num))
-    print('Price range: ' + str(price))
-#make each variable version 2
+    print(h_name.replace('-', ' ') + ' ' + str(h_loc))
+    print('Rating: ' + str(h_rating))
+    print('Number of reviews: ' + str(h_review_num))
+    print('Price range: ' + str(h_price))
 
-class Yelp:
-    def __init__(self, city= "No city", state= "No state"):
-        self.city = city
-        self.state = state
 
 
 DBNAME = 'cities.db'
@@ -230,8 +176,9 @@ def init_db():
 #make thing for yelp to select top 5 restaurants and top 5 hotel/travel
 
 
-#interactive prompt pt 1
-#user_resp = input('Enter a state abbreviation (or "help" for options): ')
+#interactive prompt
+
+init_user_inp = input('Enter a state abbreviation(type help for a list of states): ')
 state_dict = {
         'ak': 'Alaska',
         'al': 'Alabama',
@@ -260,7 +207,6 @@ state_dict = {
         'mo': 'Missouri',
         'ms': 'Mississippi',
         'mt': 'Montana',
-        'na': 'National',
         'nc': 'North Carolina',
         'nd': 'North Dakota',
         'ne': 'Nebraska',
@@ -286,7 +232,71 @@ state_dict = {
         'wv': 'West Virginia',
         'wy': 'Wyoming'
 }
+
+if init_user_inp == 'help':
+     print('Please enter one of the following state abbreviations:')
+     print('ak for Alaska')
+     print('al for Alabama')
+     print('ar for Arkansas')
+     print('az for Arizona')
+     print('ca for California')
+     print('co for Colorado')
+     print('ct for Connecticut')
+     print('dc for District of Columbia')
+     print('de for Delaware')
+     print('fl for Florida')
+     print('ga for Georgia')
+     print('hi for Hawaii')
+     print('ia for Iowa')
+     print('id for Idaho')
+     print('il for Illinois')
+     print('in for Indiana')
+     print('ks for Kansas')
+     print('ky for Kentucky')
+     print('la for Louisiana')
+     print('ma for Massachusetts')
+     print('md for Maryland')
+     print('me for Maine')
+     print('mi for Michigan')
+     print('mn for Minnesota')
+     print('mo for Missouri')
+     print('ms for Mississippi')
+     print('mt for Montana')
+     print('nc for North Carolina')
+     print('nd for North Dakota')
+     print('ne for Nebraska')
+     print('nh for New Hampshire')
+     print('nj for New Jersey')
+     print('nm for New Mexico')
+     print('nv for Nevada')
+     print('ny for New York')
+     print('oh for Ohio')
+     print('ok for Oklahoma')
+     print('or for Oregon')
+     print('pa for Pennsylvania')
+     print('ri for Rhode Island')
+     print('sc for South Carolina')
+     print('sd for South Dakota')
+     print('tn for Tennessee')
+     print('tx for Texas')
+     print('ut for Utah')
+     print('va for Virginia')
+     print('vt for Vermont')
+     print('wa for Washington')
+     print('wi for Wisconsin')
+     print('wv for West Virginia')
+     print('wy for Wyoming')
+     init_user_inp = input('Enter a state abbreviation: ') #explain to GSI only need it once
+
+elif init_user_inp in state_dict.keys():
+
+
+ else:
+     exit()
+
 #call function at the bottom
-function(url, init_user_inp)
+
+if __name__ == '__main__':
+    unittest.main()
 
 #plotly with price??
