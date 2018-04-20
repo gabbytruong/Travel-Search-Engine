39516cae93dede6232c73da7005cd9e69e600c63
Gabby Truong <gtruong@0587343518.wireless.umich.net>
2018-04-20 14:42:44 -0400

    added changes

--- finalproj.py
+++ finalproj.py
@@ -39,7 +39,7 @@ def get_cached_netstate(baseurl):
         return CACHE_DICT_3[unique_ident]
 
 #INITIAL PROMPT FOR USER INPUT
-user_inp = input('Enter a state abbreviation (or "help" for a list of states): ')
+user_inp = input('Please enter a state abbreviation (or enter "help" for a list of states): ')
 
 #ADDING A HELP OPTION
 if user_inp == 'help':
@@ -94,7 +94,7 @@ if user_inp == 'help':
      print('wi for Wisconsin')
      print('wv for West Virginia')
      print('wy for Wyoming')
-     user_inp = input('Enter a state abbreviation: ')
+     user_inp = input('Please enter a state abbreviation: ')
 
 #CREATE NETSTATE CLASS
 class NetState:
@@ -135,6 +135,7 @@ class NetState:
 
 print(' ')
 
+
 #GET NETSTATE DATA
 def get_netstate_data(user_inp):
     baseurl = 'http://www.netstate.com/states/alma/{}_alma.htm'.format(user_inp)
@@ -147,7 +148,6 @@ def get_netstate_data(user_inp):
     td = tr.find_all('td')
     city = td[1].string
     population = td[2].string
-
     loc = NetState(city = city, population = population, state_abbr = user_inp)
 
     print(city + ', ' + user_inp.upper() + ': population of ' + population)
@@ -213,75 +213,95 @@ def get_hotels(user_inp):
         print('')
 
 #SECOND USER PROMPT
-user_inp_2 = input('To view restaurant and hotel reccomendations, enter "yes". To exit, enter "exit": ')
-if user_inp_2 == 'yes':
+user_inp_2 = input('To view restaurant and hotel reccomendations in this city, enter "view".\nTo exit the program, enter "exit": ')
+
+if user_inp_2 == 'view':
     get_restaurants(user_inp)
     get_hotels(user_inp)
-else:
+elif user_inp_2 == 'exit':
+    print ('\nThanks, and happy travels!')
     exit()
+else:
+    print('Please enter a valid command.')
+    user_inp_2 = input('To view restaurant and hotel reccomendations in this city, enter "view".\nTo exit the program, enter "exit": ')
+
+
+
 
 #FORMULATE ALL NETSTATE DATA INTO CACHE
-def state_info():
-    state_dict = {
-            'ak': 'Alaska',
-            'al': 'Alabama',
-            'ar': 'Arkansas',
-            'az': 'Arizona',
-            'ca': 'California',
-            'co': 'Colorado',
-            'ct': 'Connecticut',
-            'de': 'Delaware',
-            'fl': 'Florida',
-            'ga': 'Georgia',
-            'hi': 'Hawaii',
-            'ia': 'Iowa',
-            'id': 'Idaho',
-            'il': 'Illinois',
-            'in': 'Indiana',
-            'ks': 'Kansas',
-            'ky': 'Kentucky',
-            'la': 'Louisiana',
-            'ma': 'Massachusetts',
-            'md': 'Maryland',
-            'me': 'Maine',
-            'mi': 'Michigan',
-            'mn': 'Minnesota',
-            'mo': 'Missouri',
-            'ms': 'Mississippi',
-            'mt': 'Montana',
-            'nc': 'North Carolina',
-            'nd': 'North Dakota',
-            'ne': 'Nebraska',
-            'nh': 'New Hampshire',
-            'nj': 'New Jersey',
-            'nm': 'New Mexico',
-            'nv': 'Nevada',
-            'ny': 'New York',
-            'oh': 'Ohio',
-            'ok': 'Oklahoma',
-            'or': 'Oregon',
-            'pa': 'Pennsylvania',
-            'ri': 'Rhode Island',
-            'sc': 'South Carolina',
-            'sd': 'South Dakota',
-            'tn': 'Tennessee',
-            'tx': 'Texas',
-            'ut': 'Utah',
-            'va': 'Virginia',
-            'vt': 'Vermont',
-            'wa': 'Washington',
-            'wi': 'Wisconsin',
-            'wv': 'West Virginia',
-            'wy': 'Wyoming'
-    }
+user_inp_3 = input('To further view general city population information, enter "view".\nOtherwise, enter "exit" to exit the program: ')
 
-    city_data_lst = []
-    for state in state_dict.keys():
-        state_info = get_netstate_data(state)
-        city_data_lst.append(state_info)
-    return city_data_lst
+if user_inp_3 == 'view':
+    def state_info():
+        state_dict = {
+                'ak': 'Alaska',
+                'al': 'Alabama',
+                'ar': 'Arkansas',
+                'az': 'Arizona',
+                'ca': 'California',
+                'co': 'Colorado',
+                'ct': 'Connecticut',
+                'de': 'Delaware',
+                'fl': 'Florida',
+                'ga': 'Georgia',
+                'hi': 'Hawaii',
+                'ia': 'Iowa',
+                'id': 'Idaho',
+                'il': 'Illinois',
+                'in': 'Indiana',
+                'ks': 'Kansas',
+                'ky': 'Kentucky',
+                'la': 'Louisiana',
+                'ma': 'Massachusetts',
+                'md': 'Maryland',
+                'me': 'Maine',
+                'mi': 'Michigan',
+                'mn': 'Minnesota',
+                'mo': 'Missouri',
+                'ms': 'Mississippi',
+                'mt': 'Montana',
+                'nc': 'North Carolina',
+                'nd': 'North Dakota',
+                'ne': 'Nebraska',
+                'nh': 'New Hampshire',
+                'nj': 'New Jersey',
+                'nm': 'New Mexico',
+                'nv': 'Nevada',
+                'ny': 'New York',
+                'oh': 'Ohio',
+                'ok': 'Oklahoma',
+                'or': 'Oregon',
+                'pa': 'Pennsylvania',
+                'ri': 'Rhode Island',
+                'sc': 'South Carolina',
+                'sd': 'South Dakota',
+                'tn': 'Tennessee',
+                'tx': 'Texas',
+                'ut': 'Utah',
+                'va': 'Virginia',
+                'vt': 'Vermont',
+                'wa': 'Washington',
+                'wi': 'Wisconsin',
+                'wv': 'West Virginia',
+                'wy': 'Wyoming'
+        }
 
-#(state_info())
+        city_data_lst = []
+        for state in state_dict.keys():
+            state_info = get_netstate_data(state)
+            city_data_lst.append(state_info)
+        print ('\nThanks, and happy travels!')
+        return city_data_lst
+
+elif user_inp_3 == 'exit':
+    print ('\nThanks, and happy travels!')
+    exit()
+
+else:
+    print('Please enter a valid command.')
+    user_inp_3 = input('To further view general city population information, enter "view".\nOtherwise, enter "exit" to exit the program: ')
+
+
 
 #CREATE DB WITH FOLLOWING TABLES:
 #table 'NetState' - city (w/ID), state, population (50 of them)
@@ -321,19 +341,18 @@ def init_db():
         CREATE TABLE 'YelpResults' (
             'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
             'City' TEXT NOT NULL,
-            'StateAbbr' TEXT NOT NULL,
-            'Category' TEXT NOT NULL,
-            'Rating' INTEGER,
-            'Price' INTEGER,
-            'Reviews' INTEGER
+            'StateAbbr' TEXT NOT NULL
         );
     '''
-
+            # 'Name' TEXT NOT NULL,
+            # 'Category' TEXT NOT NULL,
+            # 'Rating' INTEGER,
+            # 'Reviews' INTEGER
     cur.execute(statement)
     conn.commit()
     conn.close()
 
-def populate_ns():
+def populate_db():
     conn = sqlite.connect(DBNAME)
     cur = conn.cursor()
 
@@ -344,20 +363,46 @@ def populate_ns():
     for item in city_info:
         city = item[0]
         population = item[2]
-        stateabbr = item[2].upper()
+        stateabbr = item[1].upper()
 
         insertion = (None, city, stateabbr, population)
         statement = 'INSERT INTO "NetState" '
         statement += 'VALUES (?, ?, ?, ?)'
         cur.execute(statement, insertion)
 
+        insertion = (None, city, stateabbr)
+        statement = 'INSERT INTO "YelpResults" '
+        statement += 'VALUES (?, ?, ?)'
+        cur.execute(statement, insertion)
+
+    # f = open('yelp_cache.json', 'r')
+    # contents = f.read()
+    # yelp_res_data = json.loads(contents)
+    # for x in yelp_res_data:
+    #     res_info = get_restaurants(x)
+    #     for loc in res_info:
+    #         city = city_info()[0][0]
+    #         stateabbr = loc
+    #         category = "restaurant"
+    #         name = loc["businesses"][x]["alias"]
+    #         rating = loc["businesses"][x]["rating"]
+    #         reviews = loc["businesses"][x]["review_count"]
+    #
+    #         insertion = (None, city, stateabbr, name, category, rating, reviews)
+    #         statement = 'INSERT INTO "YelpResults"'
+    #         statement += 'VALUES (?, ?, ?, ?)'
+    #         cur.execute(statement, insertion)
+
+    # f = open('yelp_cache_2.json', 'r')
+    # contents = f.read()
+    # yelp_hot_data = json.loads(contents)
+    #for x in yelp_hot_data:
+
     conn.commit()
     conn.close()
 
 init_db()
-populate_ns()
-
-
+populate_db()
 
 
 #plotly
