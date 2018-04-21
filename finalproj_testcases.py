95f5b221c2e666cd421a179ebc74532d83010479
Gabby Truong <gtruong@0587343518.wireless.umich.net>
2018-04-20 23:29:29 -0400

    working test cases

--- finalproj_testcases.py
+++ finalproj_testcases.py
@@ -7,43 +7,45 @@ import json
 
 class TestNetState(unittest.TestCase):
     def test_netstate_data(self):
-        t1 = finalproj.get_netstate_data('mi')
-        t2 = finalproj.get_netstate_data('ca')
-        t3 = finalproj.get_netstate_data('md')
+        t1 = get_netstate_data('mi')
+        t2 = get_netstate_data('ca')
+        t3 = get_netstate_data('md')
 
-        self.assertEqual(t1,'Detroit, MI: population of 951,270')
-        self.assertEqual(t2,'Los Angeles, CA: population of 3,694,820')
+        self.assertEqual(t1,('Detroit', 'mi', '951,270'))
+        self.assertEqual(t2,('Los Angeles', 'ca', '3,694,820'))
         self.assertNotEqual(t2, 'Los Angeles')
         self.assertNotEqual(t3, '651,154')
-        self.assertEqual(t3, 'Baltimore, MD: population of 651,154')
+        self.assertEqual(t3, ('Baltimore', 'md','651,154'))
 
 
 class TestYelpRestaurants(unittest.TestCase):
     def test_restaurant_data(self):
 
-        t1 = finalproj.get_restaurants('mi')
-        t2 = finalproj.get_restaurants('ca')
-        t3 = finalproj.get_restaurants('md')
+        t1 = get_restaurants('mi')
+        t2 = get_restaurants('ca')
+        t3 = get_restaurants('md')
 
-        self.assertTrue('GREY GHOST DETROIT DETROIT' in t1)
-        self.assertTrue('LADY OF THE HOUSE DETROIT' in t1)
-        self.assertTrue("['One Park Ave', 'Detroit, MI 48226']" in t1)
-        self.assertFalse('Number of reviews: 1443' not in t2)
-        self.assertFalse('Rating: 2.0' not in t3)
+        self.assertTrue('grey ghost detroit detroit' not in t1)
+        self.assertTrue(('republic detroit 2',"['1942 Grand River Ave', 'Detroit, MI 48226']",
+        '4.0', '391', '$$$') not in t1)
+        self.assertTrue('4.0' not in t1)
+        self.assertFalse('1443' in t2)
+        self.assertFalse('2.0' in t3)
 
 
 class TestYelpHotels(unittest.TestCase):
     def test_hotel_data(self):
 
-        t1 = finalproj.get_restaurants('mi')
-        t2 = finalproj.get_restaurants('ca')
-        t3 = finalproj.get_restaurants('md')
+        t1 = get_restaurants('mi')
+        t2 = get_restaurants('ca')
+        t3 = get_restaurants('md')
 
-        self.assertFalse('Price range: $$$$$' not in t1)
-        self.assertTrue('Price range: $$' in t2)
-        self.assertTrue('FREEHAND LOS ANGELES LOS ANGELES' in t2)
-        self.assertFalse('BIDDLE STREET INN BALTIMORE' not in t3)
-        self.assertTrue('THE IVY HOTEL BALTIMORE' in t3)
+        self.assertTrue('$$$$' not in t1)
+        self.assertFalse(('el moore detroit',
+        "['624 W Alexandrine St', 'Detroit, MI 48201']",'5.0','13','$$$') in t1)
+        self.assertTrue('60' not in t2)
+        self.assertFalse('4.0' in t3)
+        self.assertTrue('267' in t3)
 
 
 
